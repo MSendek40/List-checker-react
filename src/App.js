1e43c89700d830f6c97a798b96be994d28f29405
Marek Sendek <marek.sendek@gmail.com>
2023-01-18 17:14:06 +0100

    objects in array was deleted

--- src/App.js
+++ src/App.js
@@ -10,11 +10,7 @@ import Container from './Container';
 function App() {
   const [hideDone, setHideDone] = useState(false);
   const [tasks, setTasks] = useState(
-    [ 
-    { id: 1, content: "przejsc na reacta", done: false },
-    { id: 2, content: "umyc samochod", done: false },
-    { id: 3, content: "zrobić zakupy", done: true },
-    { id: 4, content: "napisac list", done: true },
+    [
   ]
   )
 
