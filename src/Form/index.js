8dae120a64fa523ff8ef79b4bb4d2363eb715d50
Marek Sendek <marek.sendek@gmail.com>
2023-01-18 13:29:08 +0100

    form modification

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 
-const Form = () => (
-    <form className="form__elements">
-        <input className="form__input"
-            placeholder="Co jest do zrobienia?"
-            autofocus />
-        <button className="form__button">Dodaj zadanie </button>
-    </form>
-)
+const Form = ({addNewTask}) => {
+
+    const [newTaskContent, setNewTaskContent] = useState("");
+
+const onFormSubmit =(event) => {
+    event.preventDefault();
+    addNewTask(newTaskContent.trim());
+}
+
+return (<form 
+onSubmit={onFormSubmit}
+className="form__elements">
+<input 
+value={newTaskContent} 
+className="form__input"
+    placeholder="Co jest do zrobienia?"
+    autofocus
+    onChange={(target) => setNewTaskContent(target.value)}
+    />
+<button className="form__button">Dodaj zadanie </button>
+</form>)
+    
+    
+}
+
+
+
 
 export default Form;
