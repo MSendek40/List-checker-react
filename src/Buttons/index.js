4f87e139cc8afef78745b9ab214fd7500882321a
Marek Sendek <marek.sendek@gmail.com>
2023-01-18 10:38:11 +0100

    togglehideDone added

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = (props) => {
-    if (props.tasks.length === 0) {
-        return null
-    }
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+    <div className="tasks__buttons">
 
-    return (
-        <div className="tasks__buttons">
-            <button className="buttons__button">
-                {props.hideDone ? "Pokaż" : "Ukryj"} ukończone
-            </button>
-            <button className="buttons__button"
-                disabled={props.tasks.every(({ done }) => done)}
-            >
-                Ukończ wszystkie
-            </button>
-        </div>);
-}
+        {tasks.length > 0 && (
+            <>
+                <button onClick={toggleHideDone} className="buttons__button">
+                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                </button>
+                <button
+                    className="buttons__button"
+                    disabled={tasks.every(({ done }) => done)}
+                >
+                    Ukończ wszystkie
+                </button></>
+        )}
+    </div>
+)
+
+
 
 
 export default Buttons;
