b916ef4dc239e942c3bedcc1b67cda2309e09270
Marek Sendek <marek.sendek@gmail.com>
2023-01-20 12:53:41 +0100

    classes delated in Section style.css

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
-    <section className="tasks">
-        <div className="tasks__listItem">
-            <h2 className="tasks__header" > {title} </h2>
+    <section className="section">
+        <div className="section__listItem">
+            <h2 className="section__header" > {title} </h2>
             {extraHeaderContent}
         </div>
         <div>
