a2d94959a5a96405e2aee03f8639423265d389c0
Marek Sendek <marek.sendek@gmail.com>
2023-02-15 12:39:32 +0100

    deleting files

--- src/Container/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Containerstyle } from "./styled";
-
-const Container = (props) => (
-    <Containerstyle> {props.children} </Containerstyle>
-);
-
-export default Container
--- src/Container/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Containerstyle = styled.main`
-    max-width: 900px;
-    margin: 0 auto;
-    padding: 0px 20px;
-
-`;
-
