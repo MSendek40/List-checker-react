a38795eb27eb5da3d0d621597e10f9b666ec2dd4
Marek Sendek <marek.sendek@gmail.com>
2023-02-20 11:39:02 +0100

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Tasks from '../src/features/tasks/Tasks';
+import App from "./features/tasks/App";
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyle } from './GlobalStyle';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
     <GlobalStyle />  
-    <Tasks />
+    <App />
   </React.StrictMode>
 );
 
