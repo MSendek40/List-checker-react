ee5e87e0d30b6318cafd4d5a712ad70ab2c26b7e
Marek Sendek <marek.sendek@gmail.com>
2023-03-01 20:28:30 +0100

    react-router dom 5.3.3 installed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+import { BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
-export default () => <Tasks />;
+export default () => (
+    <BrowserRouter>
+        <nav>
+            <ul>
+                <li><Link to="/zadania">
+                    Zadania
+                </Link>
+                </li>
+                <li> <Link to="/author"> O autorze </Link>
+
+                </li>
+            </ul>
+            <Switch> 
+                <Route path ="/zadania">
+                <Tasks />
+                </Route>
+                <Route path ="/author">
+                 <Author/>
+                </Route>
+            </Switch>
+        </nav>
+    </BrowserRouter>
+)
---
+++ src/features/author/Author.js
@@ -0,0 +1,21 @@
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+function Author () {
+
+<Container>
+<Header  title="O autorze"/>
+<Section title= "Marek Aleksander Sendek"
+         body= {"Cześć, witam Cię na mojej stronie"}   
+>
+
+</Section>
+</Container>
+     
+    
+
+
+}
+
+export default Author;
