2727e085c9ea6663a251e946f1585cf96aef70ad
Marek Sendek <marek.sendek@gmail.com>
2023-03-03 11:47:11 +0100

    changes in Author section

--- src/features/author/Author.js
+++ src/features/author/Author.js
@@ -1,15 +1,22 @@
 import Container from "../../common/Container";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
+import {AuthorText} from "./styled";
 
 function Author() {
     return (
         <Container>
             <Header title="O autorze" />
-            <Section title="Marek Aleksander Sendek"
-                body={
-                    <> czesc</>}
-            >
+            <Section 
+            title="Marek Aleksander Sendek"
+            body={
+                <>
+                    <AuthorText>
+                     Od 15 lat zajmuje się sprzedażą. 
+                     Moje doświadczenie zawodowe obejmuje kilka branż w tym głównie nieruchomości i przemysł farmceutyczny       
+                    </AuthorText>
+                </>
+            }>
             </Section>
         </Container>
 
---
+++ src/features/author/styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AuthorText = styled.div`
+line-height: 1.5;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+background-color: white;
+color: black;
+margin-top: 5px;
+padding: 10px;
+width: auto;
+`;
+
